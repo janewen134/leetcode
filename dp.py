d8304bd0f62404445f91977af0d927178801b9ce
Jing Wen <jing@dyn3184-52.wlan.ic.ac.uk>
2022-08-04 19:11:09 +0100

    house robber

--- dp.py
+++ dp.py
@@ -1,13 +1,48 @@
 import numpy as np
-def climb(n):
+# O(n) space complexity
+def climb_stairs(n):
+    # edge case
     if (n <= 2):
         return n
+    # init
     dp = [1] * (n+1)
+    # state update
     for i in range(2,n+1):
         dp[i] = dp[i-1] + dp[i-2]
-    print(dp)
+    # answer
+    # print(dp)
     return dp[n]
-
+# O(1) space complexity
+def climb_stairs2(n):
+    # edge case    
+    if (n<=2):
+        return n
+    # one step before, and two steps before
+    last_1, last_2 = 2, 1
+    current = 0
+    for i in range(2, n):
+        current = last_1 + last_2
+        last_2 = last_1
+        last_1 = current
+        # print('i: ',i)
+        # print(last_2)
+        # print(last_1)
+        # print(current)
+    return current
+   
+def rob_house(n):
+    if len(n) == 0:
+        return 0
+    
+    last_1,last_2 = 0,0
+    curr = 0
+    for i in n:
+        curr = max(last_1, i+last_2)
+        last_2=last_1
+        last_1=curr
+    print(curr)
+    return curr
 if __name__ == "__main__":
-    a = climb(5)
+    # a = climb_stairs2(5)
+    a = rob_house([1])
     print(a)
